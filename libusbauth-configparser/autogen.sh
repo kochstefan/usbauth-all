298779d165970e88d5ba4f1e7a9a6d03c58f876e
Stefan Koch <stefan.koch10@gmail.com>
2017-10-07 23:10:05 +0200

    use autoreconf to generate needed files for automake to build libusbauth-configparser

---
+++ libusbauth-configparser/autogen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Copyright (C) 2017 Stefan Koch <stefan.koch10@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 2.1 of the GNU Lesser General
+# Public License as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+
+autoreconf -f -i
